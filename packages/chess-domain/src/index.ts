59e97c24a80d3a4ab7658c21c38ea308f457a492
vokerg <vokerg@gmail.com>
2026-09-11 06:31:55 +0200

    feat(engine): export analysis domain helpers

--- packages/chess-domain/src/index.ts
+++ packages/chess-domain/src/index.ts
@@ -1,4 +1,6 @@
+export * from './engine-evaluation';
 export * from './game-reconstruction';
+export * from './move-classification';
 export * from './position';
 
 export const chessDomainPackage = 'why-i-suck-at-chess/chess-domain' as const;
