c7fb6c4173128fe6e83f2ffe6cd223190d7a68ce
vokerg <vokerg@gmail.com>
2026-09-12 13:37:24 +0200

    feat: export phase classification helpers

--- packages/chess-domain/src/index.ts
+++ packages/chess-domain/src/index.ts
@@ -2,6 +2,7 @@ export * from './engine-evaluation';
 export * from './game-reconstruction';
 export * from './material';
 export * from './move-classification';
+export * from './phase';
 export * from './position';
 
 export const chessDomainPackage = 'why-i-suck-at-chess/chess-domain' as const;
